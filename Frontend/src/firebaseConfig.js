291ae12cf567b84a9a4fb176132b505189dd3035
Charuni Fernando <charunifernandz@gmail.com>
2025-04-04 16:34:20 +0530

    update firebase
    update firebase

--- Frontend/src/firebaseConfig.js
+++ Frontend/src/firebaseConfig.js
@@ -1,18 +1,23 @@
+// Import necessary functions from Firebase SDK
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCDP_N9fYjlJnshuc4jckOP5IMo0HSrbmo",
-  authDomain: "pafmain-a0be6.firebaseapp.com",
-  projectId: "pafmain-a0be6",
-  storageBucket: "pafmain-a0be6.appspot.com",
-  messagingSenderId: "136832715572",
-  appId: "1:136832715572:web:ac62d5fc2f9bb35cfff361"
+  apiKey: "AIzaSyCjuWxp-qbmDPJj0JePxhzB66gzJAPmcLI",
+  authDomain: "skill-sharing-platform-df39d.firebaseapp.com",
+  projectId: "skill-sharing-platform-df39d",
+  storageBucket: "skill-sharing-platform-df39d.firebasestorage.app",
+  messagingSenderId: "6810545495",
+  appId: "1:6810545495:web:7ab740cecf22bc62dd6212",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore, Auth, and Storage services
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
