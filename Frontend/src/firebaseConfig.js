f7a75c7c4f0990745d115cb0b74cc3021e944c46
Charuni Fernando <charunifernandz@gmail.com>
2025-04-04 22:19:14 +0530

    up
    up

--- Frontend/src/firebaseConfig.js
+++ Frontend/src/firebaseConfig.js
@@ -1,23 +1,24 @@
-// Import necessary functions from Firebase SDK
+// src/firebaseConfig.js
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
+// Your Firebase config details (project specific details)
 const firebaseConfig = {
-  apiKey: "AIzaSyCjuWxp-qbmDPJj0JePxhzB66gzJAPmcLI",
-  authDomain: "skill-sharing-platform-df39d.firebaseapp.com",
-  projectId: "skill-sharing-platform-df39d",
-  storageBucket: "skill-sharing-platform-df39d.appspot.com",
-  messagingSenderId: "6810545495",
-  appId: "1:6810545495:web:7ab740cecf22bc62dd6212",
+  apiKey: "AIzaSyCjuWxp-qbmDPJj0JePxhzB66gzJAPmcLI",  // apiKey
+  authDomain: "skill-sharing-platform-df39d.firebaseapp.com", // authDomain
+  projectId: "skill-sharing-platform-df39d",  // projectId
+  storageBucket: "skill-sharing-platform-df39d.appspot.com", // storageBucket
+  messagingSenderId: "6810545495",  // messagingSenderId
+  appId: "1:6810545495:web:7ab740cecf22bc62dd6212",  // appId
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firestore, Auth, and Storage services
-export const db = getFirestore(app);
-export const auth = getAuth(app);
-export const storage = getStorage(app);
+// Firebase services
+export const db = getFirestore(app);    // Firestore
+export const auth = getAuth(app);        // Auth
+export const storage = getStorage(app);  // Storage
