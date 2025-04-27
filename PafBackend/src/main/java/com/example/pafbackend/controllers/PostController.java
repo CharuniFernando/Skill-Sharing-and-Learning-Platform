bcd379baa881d95ad1c6000edf25c3d5b9b847b1
Charuni Fernando <charunifernandz@gmail.com>
2025-04-27 15:50:56 +0530

    update post controller: HTTP status
    update post controller: HTTP status

--- PafBackend/src/main/java/com/example/pafbackend/controllers/PostController.java
+++ PafBackend/src/main/java/com/example/pafbackend/controllers/PostController.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 import java.util.List;
 
 @RestController //return data
-@RequestMapping("/api/posts") ////base URL or base path
+@RequestMapping("/api/posts") //base URL or base path
 public class PostController {
 
     //autowired repository and inject constructor
@@ -33,21 +33,21 @@ public class PostController {
     @GetMapping("/{userId}")
     public ResponseEntity<List<Post>> getPostsByUserId(@PathVariable String userId) {
         List<Post> posts = postRepository.findByUserId(userId);
-        return new ResponseEntity<>(posts, HttpStatus.OK);
+        return new ResponseEntity<>(posts, HttpStatus.OK); //HttpStatus.OK 200 - success
     }
 
     // POST: Create a new Post
     @PostMapping
     public ResponseEntity<Post> createPost(@RequestBody Post post) {
         Post savedPost = postRepository.save(post);
-        return new ResponseEntity<>(savedPost, HttpStatus.CREATED);
+        return new ResponseEntity<>(savedPost, HttpStatus.CREATED); //HttpStatus.CREATED 201 - response
     }
 
     // DELETE: Delete a Post by ID
     @DeleteMapping("/{postId}")
     public ResponseEntity<Void> deletePost(@PathVariable String postId) {
         postRepository.deleteById(postId);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT); //HttpStatus.NO_CONTENT 204 - send status without response body
     }
 
     // // PUT: Update a Post by ID
@@ -70,7 +70,7 @@ public class PostController {
             Post savedPost = postRepository.save(existingPost);
             return new ResponseEntity<>(savedPost, HttpStatus.OK);
         } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND); //HttpStatus.OK 200 - success
         }
     }
 }
